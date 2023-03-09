8bbd6fc9301aefdc98a9dfa8d714f866adee2e8a
Ibraheem <iejibode@gmail.com>
2023-03-09 22:39:00 +0100

    my commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import Hero from './Components/Hero';
+import Navbar from './Components/Navbar';
+import About from './Components/About';
+import Tours from './Components/Tours';
+import Services from './Components/Services';
+import Footer from './Components/Footer';
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <About />
+      <Services />
+      <Tours />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
